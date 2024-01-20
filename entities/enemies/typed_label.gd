1e451650f409f8b0a3bc2dc0307eee6ec9381881
LiquidFun <brutenis@gmail.com>
2024-01-20 03:57:11 +0100

    Fix that labels with weird letters can be typed

--- entities/enemies/typed_label.gd
+++ entities/enemies/typed_label.gd
@@ -4,6 +4,21 @@ signal correctly_typed(Node2D)
 
 @export var color: Color = Color.RED
 
+var map_weird_chars = {
+	"â": "a",
+	"ä": "a",
+	"é": "e",
+	"è": "e",
+	"ê": "e",
+	"ē": "e",
+	"ʻ": "'",
+	"`": "'",
+	"ô": "o",
+	"ö": "o",
+	"ō": "o",
+	"ü": "u",
+}
+
 func _ready():
 	$Typed.visible_characters = 0
 	$Typed.modulate = color
@@ -12,12 +27,22 @@ func _ready():
 func set_text(text: String):
 	$Label.text = text
 	$Typed.text = text
+	
+func normalized_string(string: String):
+	string = string.to_lower()
+	for i in range(len(string)):
+		string[i] = map_weird_chars.get(string[i], string[i])
+	return string.strip_edges()
+
 
 func type_text(typed: String) -> bool:
-	if $Label.text.begins_with(typed):
+	var text = normalized_string($Label.text)
+	typed = normalized_string(typed)
+	if text.begins_with(typed):
 		$Typed.visible_characters = len(typed)
 	else:
 		$Typed.visible_characters = 0
-	if $Label.text == typed:
+	if text == typed:
 		emit_signal("correctly_typed", self)
-	return $Label.text == typed
+		return true
+	return false
