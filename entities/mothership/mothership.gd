170f629c61fad5a6050714f94467d9c943bd25d9
Waringh4m <82370122+Waringh4m@users.noreply.github.com>
2024-01-20 13:07:47 +0100

    health_bar_in_progress

--- entities/mothership/mothership.gd
+++ entities/mothership/mothership.gd
@@ -5,12 +5,15 @@ signal player_death
 var max_hp = 1000
 var hp = 1000
 
+@onready var health_sprite = $HealthSpriteModulate
+var base_color = Color.AQUAMARINE
 
 func _ready():
 	var initial_pos = position
 	self.position.y += 400
 	create_tween().tween_property(self, "position", initial_pos, 1) \
 		.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	modulate_health_sprite()
 	
 
 
@@ -27,6 +30,7 @@ func display_impact_warning(projectile_target: Node2D, weapon: Node2D, language:
 func reduce_hp(val: int) -> void:
 	self.hp -= val
 	self.hp = clamp(self.hp, 0, max_hp)
+	modulate_health_sprite()
 	if self.hp == 0:
 		self.handle_death()
 
@@ -34,7 +38,12 @@ func reduce_hp(val: int) -> void:
 func heal(val: int) -> void:
 	self.hp += val
 	self.hp = clamp(self.hp, 0, max_hp)
+	modulate_health_sprite()
 
+
+func modulate_health_sprite():
+	var darkened_factor = 1 - (float(hp)/float(max_hp))
+	health_sprite.modulate = base_color.darkened(0.8 * darkened_factor)
 
 func handle_death() -> void:
 	player_death.emit()
