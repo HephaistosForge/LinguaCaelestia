71923055747d867f9df31dcc34e9774c2faf63e7
Waringh4m <82370122+Waringh4m@users.noreply.github.com>
2024-01-20 02:06:16 +0100

    add warning icon, add shield asset add incoming missile warning

---
+++ IncomingMissileWarning.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var icon = $Icon
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(icon, "scale", Vector2(1.3, 1.3), 0.7).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(icon, "scale", Vector2(1.0, 1.0), 0.7).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.set_loops()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- entities/mothership/mothership.gd
+++ entities/mothership/mothership.gd
@@ -1,4 +1,28 @@
 extends Area2D
 
+var hp = 1000
+signal death
+
 func get_projectile_targets() -> Array[Node]:
 	return $ProjectileTargets.get_children()
+
+
+func display_impact_warning(projectile_target: Node2D) -> void:
+	if not typeof(projectile_target) is Marker2D:
+		return
+	projectile_target.display_impact_warning()
+
+
+func reduce_hp(val: int) -> void:
+	self.hp -= val
+	self.hp = clamp(self.hp, 0, max_hp)
+	if self.hp == 0:
+		self.handle_death()
+
+
+func handle_death() -> void:
+	emit_signal("death")
+	self.queue_free()
+
+
+func spawn_shield(position_index)
