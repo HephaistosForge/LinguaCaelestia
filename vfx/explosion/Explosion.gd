3c8a2aa11ad0731c74fbfc65fcd521aeb2546068
Jonas Moesicke <jonasm2017@gmail.com>
2024-01-20 02:06:27 +0100

    Added Big Boom

---
+++ vfx/explosion/Explosion.gd
@@ -0,0 +1,14 @@
+extends CPUParticles2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+
+func set_dir(vector):
+	direction = vector
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
